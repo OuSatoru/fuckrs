8564bb2262bf2d903de4c9fe97f9a23b23a83e8e
Wang Cong <ousatoru@hotmail.com>
2020-02-08 21:01:48 +0800

    targz

---
+++ cookbook/targz/src/main.rs
@@ -0,0 +1,22 @@
+use std::fs::File;
+use flate2::Compression;
+use flate2::write::GzEncoder;
+use flate2::read::GzDecoder;
+use tar::Archive;
+
+fn main() -> Result<(), std::io::Error> {
+    // czf
+    // let tar_gz = File::create("archive.tar.gz")?;
+    // let enc = GzEncoder::new(tar_gz, Compression::default());
+    // let mut tar = tar::Builder::new(enc);
+    // tar.append_dir_all("shared", "D:\\Virtual\\Shared\\BaiduNetdiskDownload")?;
+    // Ok(())
+
+    // xzf
+    let path = "archive.tar.gz";
+    let tar_gz = File::open(path)?;
+    let tar = GzDecoder::new(tar_gz);
+    let mut archive = Archive::new(tar);
+    archive.unpack(".")?;
+    Ok(())
+}
